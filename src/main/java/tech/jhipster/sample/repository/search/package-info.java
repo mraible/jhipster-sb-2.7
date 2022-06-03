f1131757f4e8f8330d7acff54732adcff32d78a6
Matt Raible <matt.raible@okta.com>
2022-06-03 07:54:51 -0600

    Initial version of jhipster generated by generator-jhipster@7.8.1

---
+++ src/main/java/tech/jhipster/sample/repository/search/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring Data Elasticsearch repositories.
+ */
+package tech.jhipster.sample.repository.search;
---
+++ src/test/java/tech/jhipster/sample/IntegrationTest.java
@@ -0,0 +1,24 @@
+package tech.jhipster.sample;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import tech.jhipster.sample.JhipsterApp;
+import tech.jhipster.sample.config.AsyncSyncConfiguration;
+import tech.jhipster.sample.config.EmbeddedElasticsearch;
+import tech.jhipster.sample.config.EmbeddedSQL;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { JhipsterApp.class, AsyncSyncConfiguration.class })
+@EmbeddedElasticsearch
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
